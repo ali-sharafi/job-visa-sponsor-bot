751b9197964013229788173654294529d09330a4
root <root@vps.hostry.com>
2023-03-26 16:41:07 +0300

    chores

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 
 const getHashtags = (title) => {
     return title.toLowerCase()
@@ -23,22 +24,28 @@ const sleep = (ms) => {
 }
 
 const convertStringToDateTime = (relativeTime) => {
-    const timeUnits = {
-        hours: 60 * 60 * 1000,
-        hour: 60 * 60 * 1000,
-        minutes: 60 * 1000,
-        seconds: 1000,
-    };
-    const now = new Date();
-    const timestamp = now.getTime();
-    const match = /(\d+)\s+(\w+)\s+ago/.exec(relativeTime);
-    const value = parseInt(match[1]);
-    const unit = match[2];
-
-    const msAgo = value * timeUnits[unit];
-    const adjustedTimestamp = timestamp - msAgo;
-    const datetime = new Date(adjustedTimestamp);
-    return datetime.toISOString().replace(/T|Z/g, ' ').trim();
+    try {
+        const timeUnits = {
+            hours: 60 * 60 * 1000,
+            hour: 60 * 60 * 1000,
+            minutes: 60 * 1000,
+            seconds: 1000,
+        };
+        const now = new Date();
+        const timestamp = now.getTime();
+        const match = /(\d+)\s+(\w+)\s+ago/.exec(relativeTime);
+        const value = parseInt(match[1]);
+        const unit = match[2];
+    
+        const msAgo = value * timeUnits[unit];
+        const adjustedTimestamp = timestamp - msAgo;
+        const datetime = new Date(adjustedTimestamp);
+        return datetime.toISOString().replace(/T|Z/g, ' ').trim();
+    } catch (error) {
+        console.log('Error ocurred in converting time: ', error.message);
+        return moment().format('YYYY-MM-DD H:i:s');
+    }
+    
 
 }
 const locations = [
