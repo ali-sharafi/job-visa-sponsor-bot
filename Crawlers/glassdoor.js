b1a93ad605bab3162578ba0912ac075bdb028569
Ali Sharafi <47324261+ali-sharafi@users.noreply.github.com>
2023-04-05 04:02:35 +0000

    Remove src directory

--- Crawlers/glassdoor.js
+++ Crawlers/glassdoor.js
@@ -22,6 +22,7 @@ module.exports = async () => {
     page = browser.page;
     await setCookies();
     let jobs = await getJobs();
+    await browser.disconnect();
     await browser.close();
     browser = null;
 
